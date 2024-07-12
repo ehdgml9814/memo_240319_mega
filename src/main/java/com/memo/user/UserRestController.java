fc280bbb300ef156dcddd574b41fffa6268c25f3
DESKTOP-RE0A8DL\dhyoon <ehdgml3913@naver.com>
2024-07-13 00:30:47 +0900

    레이아웃 세팅 MEMO

---
+++ src/main/java/com/memo/user/UserRestController.java
@@ -0,0 +1,17 @@
+package com.memo.user;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserRestController {
+
+//	@PostMapping("/sign-up")
+//	public Map<String, Object> signUp() {
+//		
+//	}
+}
