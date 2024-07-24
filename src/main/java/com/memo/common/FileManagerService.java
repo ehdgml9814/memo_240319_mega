b3d1b6b0508fd1b21675209fc19c191e8a9aae39
DESKTOP-RE0A8DL\dhyoon <ehdgml3913@naver.com>
2024-07-25 01:46:30 +0900

    글 수정

--- src/main/java/com/memo/common/FileManagerService.java
+++ src/main/java/com/memo/common/FileManagerService.java
@@ -9,6 +9,9 @@ import java.nio.file.Paths;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component // 스프링 빈 등록
 public class FileManagerService {
 
@@ -47,5 +50,37 @@ public class FileManagerService {
 		// 주소는 이렇게 될 것이다. 지금은 그냥 컴퓨터에 저장만 하고 끝
 		// /images/aaaa_173409834/sun.png
 		return "/images/" + directoryName + "/" + file.getOriginalFilename();
+	}
+	
+	// 파일 삭제
+	// input: 이미지 경로
+	// output: X
+	public void deleteFile(String imagePath) { // /images/aaaa_1721311714837/leaf-8867169_1280.jpg
+		// D:\6_spring_project\memo\memo_workspace\images\aaaa_1721311714837/leaf-8867169_1280.jpg
+		
+		// FILE_UPLOAD_PATH = D:\\6_spring_project\\memo\\memo_workspace\\images/
+		// 주소에 겹치는 /images/를 지워야 함
+		Path path = Paths.get(FILE_UPLOAD_PATH + imagePath.replace("/images/", ""));
+		
+		// 삭제할 이미지 존재하는지 확인
+		if (Files.exists(path)) {
+			// 이미지 삭제
+			try {
+				Files.delete(path);
+			} catch (IOException e) {
+				log.info("[FileManagerService 파일 삭제] 삭제 실패. path:{}", path.toString());
+				return;
+			}
+			
+			// 폴더(디렉토리) 삭제
+			path = path.getParent();
+			if (Files.exists(path)) {
+				try {
+					Files.delete(path);
+				} catch (IOException e) {
+					log.info("[FileManagerService 디렉토리 삭제] 삭제 실패. path:{}", path.toString());
+				}
+			}
+		}
 	}
 }
--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -10,8 +10,14 @@ import com.memo.common.FileManagerService;
 import com.memo.post.domain.Post;
 import com.memo.post.mapper.PostMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class PostBO {
+	
+	// private Logger log = LoggerFactory.getLogger(PostBO.class);
+	// private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private PostMapper postMapper;
@@ -26,7 +32,7 @@ public class PostBO {
 		return postMapper.selectPostListByUserId(userId);
 	}
 	
-	// 글 수정
+	// 글 수정 페이지 갈 때 글 불러오기
 	// input: userId, postId
 	// output: Post or null
 	public Post getPostByUserIdPostId(int userId, int postId) {
@@ -46,5 +52,38 @@ public class PostBO {
 		}
 		
 		postMapper.insertPost(userId, subject, content, imagePath);
+	}
+	
+	// input: 파라미터들
+	// output: X
+	public void updatePostByPostId(
+			int userId, String loginId, 
+			int postId, String subject, String content, 
+			MultipartFile file) {
+		
+		// 업데이트 할 기존 글 가져오기(1. 이미지 교체 시 기존 이미지 삭제, 2. 업데이트 대상 확인)
+		Post post = postMapper.selectPostByUserIdPostId(userId, postId);
+		if (post == null) {
+			log.warn("[글 수정] post is null. userId:{}, postId:{}", userId, postId);
+			return;
+		}
+		
+		// 파일이 있으면
+		// 1) 새 이미지를 업로드
+		// 2) 1번 단계가 성공하면 기존 이미지가 있으면 삭제
+		String imagePath = null;
+		if (file != null) {
+			// 새 이미지 업로드
+			imagePath = fileManagerService.uploadFile(file, loginId);
+			
+			// 업로드 성공 시(imagePath != null) 기존 이미지가 있으면 삭제
+			if (imagePath != null && post.getImagePath() != null) {
+				// 실제 폴더와 이미지 제거
+				fileManagerService.deleteFile(post.getImagePath());
+			}
+		}
+		
+		// db 업데이트
+		postMapper.updatePostByPostId(postId, subject, content, imagePath);
 	}
 }
